048309f3d518a3bb4d580a9b75e777cd78469b86
praful-koli <prafulkoli852@gmail.com>
2026-06-09 16:46:49 +0530

    feat(route) : implemnt register feature with acesss and refrsh token

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -6,3 +6,15 @@ route.post('/login', (req, res) => {
         message:'User login successful'
     })
 })
+
+route.post('/register', (req, res) => {
+    res.status(201).json({
+        message : 'User register successfully',
+        data  : {
+            username : 'jack',
+            email : 'jack@gmail.com',
+            phone:1213323,
+            role : 'admin'
+        }
+    })
+})
