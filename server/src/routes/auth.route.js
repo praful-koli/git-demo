e7c629a61125b523e44e9d86e85a72a8b70a9e14
praful-koli <prafulkoli852@gmail.com>
2026-06-09 17:37:27 +0530

    feat(api): register api implement with access and refrsh token

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -6,3 +6,18 @@ route.post('/login', (req, res) => {
         message:'User login successful'
     })
 })
+route.post('/register', (req, res) => {
+    res.status(201).json({
+        message : 'User register successfully',
+        data  : {
+            id:'fnasjlk23kn13',
+            username : 'jack',
+            email : 'jack@gmail.com',
+            phone:1213323,
+            role : 'admin',
+            accessToken : 'asoifuuhwf1hy21uu029fj92hjfhj9y9yh912f',
+            refreshToken : 'aslkfja;lksjfasnjlkfjna;lsjfl;ajsl;fjla',
+
+        }
+    })
+})
